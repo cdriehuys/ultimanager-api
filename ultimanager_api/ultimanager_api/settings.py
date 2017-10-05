d793dd9bb918315705cc6ca8a1c91793ebefd5f7
Chathan Driehuys <chathan@driehuys.com>
2017-10-05 15:50:19 -0400

    Add CORS headers.
    Fixes #9

--- ultimanager_api/ultimanager_api/settings.py
+++ ultimanager_api/ultimanager_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third party apps
+    'corsheaders',
     'rest_framework',
 
     # Custom Apps
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CORS Configuration
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # DRF Configuration
